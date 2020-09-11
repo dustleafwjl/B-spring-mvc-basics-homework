03dcb0c9bc7e34061816878bf0be788e7732c211
dustleafwjl <wangjianglin_priv@qq.com>
2020-09-11 10:37:43 +0800

    [jianglin] feat: refactor login test

--- src/test/java/com/thoughtworks/capacity/gtb/mvc/api/UserControllerTest.java
+++ src/test/java/com/thoughtworks/capacity/gtb/mvc/api/UserControllerTest.java
@@ -161,28 +161,46 @@ class UserControllerTest {
     }
 
     @Test
-    public void should_return_bad_request_when_login_given_username_is_wrongful() throws Exception {
+    public void should_return_bad_request_when_login_given_username_is_empty() throws Exception {
         registerUser();
         String userNameIsEmpty = "{\"username\": \"\",\"password\": \"12345\"}";
         String userNameWrongful = "{\"username\": \"@Tom\",\"password\": \"12345\"}";
-        String userNameOutOfLimitWithLess = "{\"username\": \"To\",\"password\": \"12345\"}";
-        String userNameOutOfLimitWithMore = "{\"username\": \"Tom123456789\",\"password\": \"12345\"}";
 
         mockMvc.perform(get("/user")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(userNameIsEmpty))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message", is("用户名不能为空")));
+    }
+
+    @Test
+    public void should_return_bad_request_when_login_given_username_is_wrongful() throws Exception {
+        registerUser();
+        String userNameWrongful = "{\"username\": \"@Tom\",\"password\": \"12345\"}";
         mockMvc.perform(get("/user")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(userNameWrongful))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message", is("用户名不合法")));
+    }
+
+    @Test
+    public void should_return_bad_request_when_login_given_username_size_is_less() throws Exception {
+        registerUser();
+        String userNameOutOfLimitWithLess = "{\"username\": \"To\",\"password\": \"12345\"}";
+
         mockMvc.perform(get("/user")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(userNameOutOfLimitWithLess))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message", is("用户名不合法")));
+    }
+
+    @Test
+    public void should_return_bad_request_when_login_given_username_size_is_more() throws Exception {
+        registerUser();
+        String userNameOutOfLimitWithMore = "{\"username\": \"Tom123456789\",\"password\": \"12345\"}";
+
         mockMvc.perform(get("/user")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(userNameOutOfLimitWithMore))
@@ -191,22 +209,33 @@ class UserControllerTest {
     }
 
     @Test
-    public void should_return_bad_request_when_login_given_password_is_wrongful() throws Exception {
+    public void should_return_bad_request_when_login_given_password_is_empty() throws Exception {
         registerUser();
         String passWordIsEmpty = "{\"username\": \"Tom\",\"password\": \"\",\"email\": \"tom@qq.com\"}";
-        String passWordOutOfLimitWithLess = "{\"username\": \"Tom\",\"password\": \"1234\",\"email\": \"tom@qq.com\"}";
-        String passWordOutOfLimitWithMore = "{\"username\": \"Tom\",\"password\": \"1234567891012\",\"email\": \"tom@qq.com\"}";
 
         mockMvc.perform(get("/user")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(passWordIsEmpty))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message", is("密码不能为空")));
+    }
+
+    @Test
+    public void should_return_bad_request_when_login_given_password_size_less() throws Exception {
+        registerUser();
+        String passWordOutOfLimitWithLess = "{\"username\": \"Tom\",\"password\": \"1234\",\"email\": \"tom@qq.com\"}";
         mockMvc.perform(get("/user")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(passWordOutOfLimitWithLess))
                 .andExpect(status().isBadRequest())
                 .andExpect(jsonPath("$.message", is("密码不合法")));
+    }
+
+    @Test
+    public void should_return_bad_request_when_login_given_password_size_more() throws Exception {
+        registerUser();
+        String passWordOutOfLimitWithMore = "{\"username\": \"Tom\",\"password\": \"1234567891012\",\"email\": \"tom@qq.com\"}";
+
         mockMvc.perform(get("/user")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(passWordOutOfLimitWithMore))
